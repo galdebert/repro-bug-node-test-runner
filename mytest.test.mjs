c89cbc6b1200d4574a2143245f69cfac7d03103d
Guillaume Aldebert <galdebert@gmail.com>
2025-03-10 13:11:58 +0100

    code

---
+++ mytest.test.mjs
@@ -0,0 +1,13 @@
+import { test } from "node:test";
+
+test("test1", (t) => {
+  console.log(t.name);
+});
+
+test("test2", (t) => {
+  console.log(t.name);
+});
+
+test("test3", (t) => {
+  console.log(t.name);
+});
---
+++ run-isolation-process.mjs
@@ -0,0 +1,12 @@
+import * as test from "node:test";
+import { spec } from "node:test/reporters";
+
+test
+  .run({
+    globPatterns: ["mytest.test.mjs"],
+    testNamePatterns: ["test1"],
+    testSkipPatterns: ["test2"],
+    isolation: "process",
+  })
+  .compose(spec)
+  .pipe(process.stdout);
